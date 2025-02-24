f6aa5927d277b1981814dd4a836aeedd604ac1cb
a-star711 <a.krasimirov35@gmail.com>
2025-02-24 21:19:16 +0200

    update config.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const notFoundHandler = require("./middleware/not-found");
 const ratesRouter = require("./routes/rates");
 const convertRouter = require("./routes/convert");
 const sortingRouter = require("./routes/sortRates");
-const { port, mongoURI } = require("./utils/config");
+const { port, mongoURI, rateLimitObj } = require("./utils/config");
 
 const app = express();
 const connectDB = require("./db/connect");
@@ -17,19 +17,17 @@ const helmet = require("helmet");
 const xss = require("xss-clean");
 const rateLimit = require("express-rate-limit");
 
+const limiter = rateLimit(rateLimitObj);
+
 app.set("trust proxy", 1);
 
 app.use(helmet());
+app.use(limiter);
 app.use(cors());
 app.use(xss());
 app.use(express.json());
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  message: "Too many requests from this IP, please try again after 15 minutes",
-});
-app.use(limiter);
+
 
 app.use("/rates", ratesRouter);
 app.use("/convert", convertRouter);
@@ -57,7 +55,6 @@ app.use(errorHandler);
 const start = async () => {
   try {
     await connectDB(mongoURI);
-    // Uncomment to test endpoints on dev
 
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
--- utils/config.js
+++ utils/config.js
@@ -1,10 +1,17 @@
 require("dotenv").config();
 
+const port = process.env.PORT || 8082;
+
+const mongoURI = process.env.MONGO_URI;
+
+const rateLimitObj = {
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests from this IP, please try again after 15 minutes",
+};
+
 const EXCHANGE_RATE_API = {
   BASE_URL: `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`,
 };
 
-const port = process.env.PORT || 8082;
-const mongoURI = process.env.MONGO_URI || "your-default-mongo-uri";
-
-module.exports = { port, mongoURI, EXCHANGE_RATE_API };
+module.exports = { port, mongoURI, EXCHANGE_RATE_API, rateLimitObj };
