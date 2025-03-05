4e5bdfbdbc0cc89ee0e8ab5d1653542abf4ea270
a-star711 <a.krasimirov35@gmail.com>
2025-03-05 13:46:42 +0200

    minor fix

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerSpecs = require("./docs/swagger");
 
 const swaggerOptions = {
-  customCssUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
+  customCssUrl:[
+    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css"],
   customJs: [
     "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js",
     "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js",
