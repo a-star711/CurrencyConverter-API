ab619713dac40d363303df83c6671579beecf2a5
a-star711 <a.krasimirov35@gmail.com>
2025-03-05 14:22:19 +0200

    remove latest modifications
    need to think of a way how to properly serve the swagger-ui static assets to vercel
    -overriding HTML didn't worked
    -adding second swaggerOptions config didn't worked either

--- app.js
+++ app.js
@@ -17,8 +17,6 @@ const rateLimit = require("express-rate-limit");
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpecs = require("./docs/swagger");
 
-// Updated swaggerOptions: we override default assets by adding a nested swaggerOptions property.
-// Note: oauth2RedirectUrl isn't available on the server, so it's removed in this example.
 const swaggerOptions = {
   customCssUrl:
     "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
@@ -34,7 +32,7 @@ app.set("trust proxy", 1);
 
 app.use(
   helmet({
-    contentSecurityPolicy: false, // or configure as needed
+    contentSecurityPolicy: false, 
   })
 );
 
@@ -49,22 +47,6 @@ app.get("/", (req, res) => {
 
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs, swaggerOptions));
-
-app.use("/api-docs", (req, res, next) => {
-  const originalSend = res.send;
-  res.send = function (html) {
-    if (typeof html === "string") {
-      const cleanedHtml = html
-        .replace(/href="\.\/swagger-ui\.css"/g, '')
-        .replace(/src="\.\/swagger-ui-bundle\.js"/g, '')
-        .replace(/src="\.\/swagger-ui-standalone-preset\.js"/g, '');
-      return originalSend.call(this, cleanedHtml);
-    }
-    return originalSend.call(this, html);
-  };
-  next();
-});
-
 
 app.use("/api/v1/rates", ratesRouter);
 
